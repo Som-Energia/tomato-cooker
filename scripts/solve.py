cd382dee59d28634e8592784f9949680355bab8d
Oriol Piera <oriol.piera@somenergia.coop>
2022-11-03 17:34:02 +0100

    Add output conversion to yaml,

--- scripts/solve.py
+++ scripts/solve.py
@@ -13,5 +13,46 @@ def graellada(dzndata_file: str = "./exemple-setmana.dzn"):
     result = instance.solve()
     print(result.solution.graella)
 
+    '''
+    #Expected output result.solution.graella
+    output = [[{1, 2, 3, 4, 5, 6, 7, 8},
+        {9, 10, 11, 12, 13, 14, 15, 16},
+        {17, 18, 19, 20, 21},
+        {22, 23, 24, 25, 26, 27, 28, 29}],
+        [{1, 2, 3, 4, 5, 6, 7, 8},
+        {9, 10, 11, 12, 13, 14, 15, 16},
+        {17, 18, 19, 20, 21},
+        {22, 23, 24, 25, 26, 27, 28, 29}],
+        [{1, 2, 3, 4, 5, 6, 7, 8},
+        {9, 10, 11, 12, 13, 14, 15, 16},
+        {17, 18, 19, 20, 21},
+        {22, 23, 24, 25, 26, 27, 28, 29}],
+        [{1, 2, 3, 4, 5, 6, 7, 8},
+        {9, 10, 11, 12, 13, 14, 15, 16},
+        {17, 18, 19, 20, 21},
+        {22, 23, 24, 25, 26, 27, 28, 29}],
+        [{1, 2, 3, 4, 5, 6, 7, 8},
+        {9, 10, 11, 12, 13, 14, 15, 16},
+        {17, 18, 19, 20, 21},
+        {22, 23, 24, 25, 26, 27, 28, 29}]]
+    '''
+
+    nlinies = 8
+    i = 0
+    str_days = ['dl', 'dm','dx','dj','dv']
+    timetable = {'timetable': {}}
+    for day in output:
+        llista = []
+        for slot in day:
+            slot = list(slot) + [0]*(nlinies - len(slot))
+            llista.append(slot)
+        timetable['timetable'][str_days[i]] = llista
+        i += 1
+
+
+    with open('data.yml', 'w') as outfile:
+        yaml.dump(timetable, outfile, default_flow_style=False, sort_keys=False)
+
+
 if __name__ == "__main__":
 	typer.run(graellada)
