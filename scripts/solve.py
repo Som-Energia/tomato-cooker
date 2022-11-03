549399db09cf59538dea3acb66aace574f3b2c6b
raul.carballo <raul.carballo@somenergia.coop>
2022-11-03 17:39:57 +0100

    WIP executors (python script)

--- scripts/solve.py
+++ scripts/solve.py
@@ -1,17 +1,47 @@
-import typer
-
 from minizinc import Instance, Model, Solver
+import asyncio
+import minizinc
+import pymzn
+
+# exemples d'inputs
+
+indisponibilitats = [[{1},{1},{1},{1},{1}], [{4},{4},{4},{4},{4}]]
+nTorns = [3,5]
+
+with open('./test_generate.dzn', 'w') as f:
+    f.write('\n'.join(pymzn.dict2dzn({
+        'nTorns':nTorns,
+        'indisponibilitats':indisponibilitats,
+        }
+    )))
+exit()
+
+model = Model("./graellador.mzn")
+dzdata_file = './input_base.dzn'
+
+async def solver_race(model, dzdata_file, solvers):
+    model.add_file(dzdata_file)
+    tasks = set()
+    for solver in solvers:
+        # Create an instance of the model for every solver
+        inst = minizinc.Instance(solver, model)
+    
+        # Create a task for the solving of each instance
+        task = asyncio.create_task(inst.solve_async())
+        task.solver = solver.name
+        tasks.add(task)
+
+    # Wait on the first task to finish and cancel the other tasks
+    done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+    for t in pending:
+        t.cancel()
+
+    # Declare the winner
+    for t in done:
+        print("{} finished solving the problem first!".format(t.solver))
+        print("{} i la graella és... ".format(t.result().solution.ocupacioSlot))
 
 
-def graellada(dzndata_file: str = "./exemple-setmana.dzn"):
-
-    gecode = Solver.lookup("gecode")
-
-    graellador = Model("grup1/grup1_graellador.mzn")
-    graellador.add_file(dzndata_file)
-    instance = Instance(gecode, graellador)
-    result = instance.solve()
-    print(result.solution.graella)
-
-if __name__ == "__main__":
-	typer.run(graellada)
+solvers = ['chuffed', 'gecode', 'coin-bc']
+mz_solvers = [minizinc.Solver.lookup(solver) for solver in solvers]
+asyncio.run(solver_race(model, dzdata_file, mz_solvers))
